ad98f8cf8083dad54d5e8051db995f6401160b14
rodrigo <rorofer100@gmail.com>
2025-11-19 16:51:22 +0100

    Add Permisos class

---
+++ ModeloDatos/Permisos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDatos
+{
+    public enum Permisos
+    {
+        Ninguno = 0,
+        CrearEditarPlanPruebas,
+        PlanificacionPlanPruebas,
+        AsignacionRoles,
+        EditarCasosPruebaEjecutados,
+        VerCasosPrueba,
+        VerRequisitos,
+        GestionRequisitos,
+        GestionProyectoPruebas,
+        GestionInventario,
+        GestionUsuarios,
+        GestionRoles,
+        GestionEventos,
+        GestionPlataformas
+    }
+}
